994c7c0238212f7eb37b55cccbed812bda9d0de1
harsh <harsh14791@gmail.com>
2021-01-04 02:13:53 +0530

    add eval files

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
                     handlers=[LoggingHandler()])
 
 config = yaml.safe_load(open('config.yml', 'r'))
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ["TORCH_HOME"] = config['base_model_dir']
 model_name = config['base_model']
 train_batch_size = config['batch_size']
@@ -19,7 +18,7 @@ model = SentenceTransformer(model_name)
 logging.info("Processing SemCor train dataset")
 
 train_samples = []
-train_file = open(config['train_flat_file'], 'r', encoding='utf8')
+train_file = open(os.path.join(config['train_dir'], config['train_flat_file']), 'r', encoding='utf8')
 for line in train_file.readlines()[1:]:
     info = line.split('\t')
     train_samples.append(InputExample(texts=[info[2].strip(), info[3].strip()], label=int(info[1].strip())))
